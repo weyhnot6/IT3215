90c3d6f5153385db827dcd68afa193d9280bfe4b
weyhnot6 <92497488+weyhnot6@users.noreply.github.com>
2021-10-24 12:15:34 -0700

    Add files via upload

--- js/volunteers.js
+++ js/volunteers.js
@@ -3,10 +3,16 @@ var $ = function (id) { return document.getElementById(id); };
 var volunteerArray = [];
 
 var displayVolunteers = function () {   
-    // display the volunteers in the text area
-    $("volunteerList").value = volunteerArray.join("\n");
-
-	// comment out the line above change this to a loop instead to loop through the array.
+    // display the volunteers in the text area and number them as they are added
+    //$("volunteerList").value = volunteerArray.join("\n");
+    
+        var volunteerString = "";
+    
+        for (var i = 0; i < volunteerArray.length; i++){
+            volunteerString = volunteerString + "\n" + (i+1) + ". " + volunteerArray[i];
+        }
+    
+        $("volunteerList").value = volunteerString;
 	
 	
 };
@@ -29,12 +35,16 @@ var addVolunteer = function () {
 
 
 var deleteVolunteer = function () {
-    // get the data from the form (hint: use the same format as from the add).
-
-    // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array.
-	
-   
-	 
+    //get the data from the form
+   var volunteerString = $("first_name").value + " " + $("last_name").value;
+    
+    //delete name
+    for (var i = 0; i < volunteerArray.length; i++){
+        if (volunteerArray[i] === volunteerString){
+            volunteerArray.splice(i,1)
+        }
+    }
+     
     // display the volunteers and clear the add form
     displayVolunteers();
     
